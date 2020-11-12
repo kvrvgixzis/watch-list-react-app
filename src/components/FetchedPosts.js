f6b8ff696c46d6d949fc9fa90ceda150b6e64f91
Nikita Parfenov <n1koss.boy@gmail.com>
2020-11-12 21:43:21 +0500

    async fetch posts

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../redux/actions/posts';
 import { Post } from './Post';
 
-export const FetchedPosts = ({ posts }) => {
+export const FetchedPosts = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.fetchedPosts);
+
+  const clickHandler = () => {
+    dispatch(fetchPosts());
+  };
+
   if (!posts.length) {
-    return <button className="btn btn-primary">Загрузить посты</button>;
+    return (
+      <button className="btn btn-primary" onClick={clickHandler}>
+        Загрузить посты
+      </button>
+    );
   }
-  return posts.map((post) => <Post post={post} />);
+  return posts.map((post) => <Post post={post} key={post.id} />);
 };
 
 export default FetchedPosts;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { compose, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { root } from './reducers/root';
 
 export const store = createStore(
   root,
   compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
