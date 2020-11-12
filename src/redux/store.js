858a83b6f376c51dd537e2d25f247bd38c8dfd19
Nikita Parfenov <n1koss.boy@gmail.com>
2020-11-13 03:47:41 +0500

    redux bug on mobile devices wip

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import { root } from './reducers/root';
 export const store = createStore(
   root,
   compose(
-    applyMiddleware(thunk, inputFilter),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk, inputFilter)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
