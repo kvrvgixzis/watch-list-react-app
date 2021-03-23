02c0d6372f2929dfbaf0a4c5a8eeb89610ca0c1f
Nikita Parfenov <n1koss.boy@gmail.com>
2021-03-24 02:17:27 +0300

    error handling in tmdb api

--- src/api/TMDB.js
+++ src/api/TMDB.js
@@ -10,19 +10,23 @@ export const getTMDBData = async (title) => {
 
   title = title.split(' ').join('%20');
   const url = `${TMDB_SEARCH_URL}&query=${title}`;
-  const res = await fetch(url);
-  const json = await res.json();
 
-  if (!json?.results?.length) return filmData;
-  const target = json.results[0];
-  console.log('target: ', target);
+  try {
+    const res = await fetch(url);
+    const json = await res.json();
 
-  filmData.posterUrl = `http://image.tmdb.org/t/p/w500/${target.poster_path}`;
-  filmData.originalTitle = target.original_title;
-  filmData.originalName = target.original_name;
-  filmData.voteAvg = target.vote_average;
-  filmData.voteCount = target.vote_count;
-  filmData.overview = target.overview;
+    if (!json?.results?.length) return filmData;
+    const target = json.results[0];
 
-  return filmData;
+    filmData.posterUrl = `http://image.tmdb.org/t/p/w500/${target.poster_path}`;
+    filmData.originalTitle = target.original_title;
+    filmData.originalName = target.original_name;
+    filmData.voteAvg = target.vote_average;
+    filmData.voteCount = target.vote_count;
+    filmData.overview = target.overview;
+  } catch (error) {
+    console.warn('[TMDB] error: ', error);
+  } finally {
+    return filmData;
+  }
 };
