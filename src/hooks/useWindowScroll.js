ed59b8b9e33c87ef1af60c04f763eed28f8060c2
Nikita Parfenov <n1koss.boy@gmail.com>
2021-03-24 03:01:43 +0300

    create use window scroll hook

---
+++ src/hooks/useWindowScroll.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+/**
+ * Add handlers to window scroll
+ * @param {{
+ *  onScrollDown: function,
+ *  onScrollUp: function
+ * }} props
+ */
+export const useWindowScroll = ({
+  onScrollDown = () => {},
+  onScrollUp = () => {},
+}) => {
+  let lastScrollTop = 0;
+
+  const scrollHandler = () => {
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    st > lastScrollTop ? onScrollDown() : onScrollUp();
+    lastScrollTop = st <= 0 ? 0 : st;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
