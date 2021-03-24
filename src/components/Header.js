af871c548c58cf2ce150bc08e5a71aebe5df95c6
Nikita Parfenov <n1koss.boy@gmail.com>
2021-03-24 03:02:13 +0300

    make header fixed on scroll up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,47 @@
+import '../styles/Header.css';
+
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useWindowScroll } from '../hooks/useWindowScroll';
 import { showAddForm } from '../redux/actions/app';
-import '../styles/Header.css';
 import { Button } from './Button';
+import { useRef } from 'react';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const DevBlock = () => {
+  if (!isDev) return null;
+
+  return (
+    <div className="dev-block">
+      You are running this application in <b>{process.env.NODE_ENV}</b> mode.
+    </div>
+  );
+};
+
 export const Header = () => {
   const dispatch = useDispatch();
+  const headerRef = useRef();
+
+  const setHeaderFixed = () => {
+    headerRef.current.classList.add('fixed');
+  };
+
+  const unsetHeaderFixed = () => {
+    headerRef.current.classList.remove('fixed');
+  };
+
+  useWindowScroll({
+    onScrollDown: unsetHeaderFixed,
+    onScrollUp: setHeaderFixed,
+  });
 
   const addBtnHandler = () => {
-    console.log('Hello');
     dispatch(showAddForm());
   };
 
-  const DevBlock = () => {
-    if (!isDev) return null;
-
-    return (
-      <div className="dev-block">
-        You are running this application in <b>{process.env.NODE_ENV}</b> mode.
-      </div>
-    );
-  };
-
   return (
-    <header className="header">
+    <header ref={headerRef} className="header">
       <DevBlock />
       <div className="header__body">
         <div className="header__title">
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,13 +1,30 @@
+import '../styles/MainLayout.css';
+
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { Alert } from '../components/Alert';
 import { AddForm } from '../components/AddForm';
-
-import '../styles/MainLayout.css';
+import { useRef } from 'react';
+import { useWindowScroll } from '../hooks/useWindowScroll';
 
 export const MainLayout = ({ children }) => {
+  const containerRef = useRef();
+
+  const setHeaderFixed = () => {
+    containerRef.current.classList.add('fixed');
+  };
+
+  const unsetHeaderFixed = () => {
+    containerRef.current.classList.remove('fixed');
+  };
+
+  useWindowScroll({
+    onScrollDown: unsetHeaderFixed,
+    onScrollUp: setHeaderFixed,
+  });
+
   return (
-    <div className="container">
+    <div ref={containerRef} className="container">
       <Header />
       <Alert />
       <AddForm />
